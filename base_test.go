deff30f557d4a5c779d292c5aee663f7d63768e4
liut <liut@phoenixtv.com>
2019-10-27 17:37:10 +0800

    more test

--- base_test.go
+++ base_test.go
@@ -15,12 +15,13 @@ var pairs = []testpair{
 	{"297eb4fe7f4993", "37051e7sxw3", 16, 36},
 	{"49262201ced289", "5mqek115cax", 16, 36},
 	{"12cfe6fab9b713e", "n67j3ox4qpq", 16, 36},
+	{"dd0274386e26030c", "3czs1w1ul6cr0", 16, 36},
+	{"dd0274386e26030c0301", "4pyem417r86neold", 16, 36},
 	{"5cc163b92ab9b482b4486999d354f91e", "5hos8aw6atq7kcpvn1gweaf4u", 16, 36},
 	{"5cc163b92ab9b482b4486999d354f91e", "2P1FKmvE5PjCN4PhpocjBs", 16, 62},
 	{"d1fb1bc11d2e992b4be5f770f35e345aa75a1d11", "oj0fiwpthr2v1ecf3p27ktokmh1a51t", 16, 36},
 	{"d1fb1bc11d2e992b4be5f770f35e345aa75a1d11", "tXzMTsS55hMM9DEUf4BXSlq509b", 16, 62},
 	{"d1fb1bc11d2e992b4be5f770f35e345aa75a1d1104", "1ZHZLT1diZProPNKUef2LeorGEBTu", 16, 62},
-	{"1ZHZLT1diZProPNKUef2LeorGEBTu", "d1fb1bc11d2e992b4be5f770f35e345aa75a1d1104", 62, 16},
 }
 
 func testEqual(t *testing.T, msg string, args ...interface{}) bool {
